fad59650cbeb23923a4fa5a78e25c64d13de8c2a
markg2000 <mrk3600@gmail.com>
2022-11-27 14:13:05 +0100

    exercises 1 to 5

--- Exercise1/src/DateTime.hs
+++ Exercise1/src/DateTime.hs
@@ -1,5 +1,6 @@
 module DateTime where
 
+import Data.Time.Calendar (gregorianMonthLength)
 import ParseLib.Abstract
 import Prelude hiding (sequence, ($>), (*>), (<$), (<*))
 
@@ -41,61 +42,75 @@ newtype Second = Second {runSecond :: Int} deriving (Eq, Ord)
 parseDateTime :: Parser Char DateTime
 parseDateTime = DateTime <$> parseDate <* (symbol 'T') <*> parseTime <*> parseUtc
 
-
-
 parseDate :: Parser Char Date
 parseDate = Date <$> parseYear <*> parseMonth <*> parseDay
   where
-    parseYear = (\x y z q -> Year (x*1000 + y*100 + z *10 + q)) <$> newdigit <*> newdigit <*> newdigit <*> newdigit
-    parseMonth = (\x y -> Month (x*10 + y)) <$> newdigit <*> newdigit
-    parseDay = (\x y -> Day (x*10 + y)) <$> newdigit <*> newdigit
+    parseYear = (\x y z q -> Year (x * 1000 + y * 100 + z * 10 + q)) <$> newdigit <*> newdigit <*> newdigit <*> newdigit
+    parseMonth = (\x y -> Month (x * 10 + y)) <$> newdigit <*> newdigit
+    parseDay = (\x y -> Day (x * 10 + y)) <$> newdigit <*> newdigit
 
 parseTime :: Parser Char Time
 parseTime = Time <$> parseHour <*> parseMinute <*> parseSecond
   where
     -- parseHour :: Hour
-    --parseHour = (\x  -> Hour (read [x] )) <$> digit <* digit
-    parseHour = (\x y -> Hour (x*10 + y)) <$> newdigit <*> newdigit
-    parseMinute = (\x y -> Minute (x*10 + y)) <$> newdigit <*> newdigit
-    parseSecond = (\x y -> Second (x*10 + y)) <$> newdigit <*> newdigit
-
+    -- parseHour = (\x  -> Hour (read [x] )) <$> digit <* digit
+    parseHour = (\x y -> Hour (x * 10 + y)) <$> newdigit <*> newdigit
+    parseMinute = (\x y -> Minute (x * 10 + y)) <$> newdigit <*> newdigit
+    parseSecond = (\x y -> Second (x * 10 + y)) <$> newdigit <*> newdigit
 
 parseUtc :: Parser Char Bool
-parseUtc = const True <$> symbol 'z'
-            <|> const False <$> epsilon
+parseUtc =
+  const True <$> symbol 'Z'
+    <|> const False <$> epsilon
 
 -- Exercise 2
 run :: Parser a b -> [a] -> Maybe b
-run parser xs | null p = Nothing
-              | otherwise = Just (fst (head p))
+run parser xs
+  | null p = Nothing
+  | otherwise = Just (fst (head p))
   where
-    p = filter (\(_,s) -> null s) (parse parser xs) 
-
-
--- = Just $ fst $ head (filter (\(_,s) -> null s) (parse parser xs) )
-  -- case parse parser xs of
-  --   ((res,_):_) -> Just res
-  --   _ -> Nothing
+    p = filter (\(_, s) -> null s) (parse parser xs)
 
 -- Exercise 3
 printDateTime :: DateTime -> String
-printDateTime (DateTime (Date (Year y) (Month mon) (Day d)) (Time (Hour h) (Minute min) (Second s)) u) = 
-  "year=" ++ show y ++ " month=" ++show mon ++ " day=" ++show d ++ 
-  "T" ++ " hour=" ++ show h ++ " minutes=" ++ show min ++ " seconds=" ++ show s ++ " utc=" ++showUTC
-    where
-       showUTC :: String
-       showUTC | u = "Z"
-               | otherwise = "NoUTC"
+printDateTime (DateTime (Date (Year y) (Month mon) (Day d)) (Time (Hour h) (Minute min) (Second s)) u) =
+  showNum y
+    ++ showNum mon
+    ++ showNum d
+    ++ "T"
+    ++ showNum h
+    ++ showNum min
+    ++ showNum s
+    ++ showUTC
+  where
+    showUTC :: String
+    showUTC
+      | u = "Z"
+      | otherwise = ""
 
-printDate :: Time -> String
-printDate (Time (Hour h) (Minute min) (Second s)) = " hour=" ++ show h ++ " minutes=" ++ show min ++ " seconds=" ++ show s
-
-parsePrintDate s = fmap printDate $ run parseTime s
-
+    showNum :: Int -> String
+    showNum x
+      | x < 10 = "0" ++ show x
+      | otherwise = show x
 
 -- Exercise 4
 parsePrint s = fmap printDateTime $ run parseDateTime s
 
 -- Exercise 5
 checkDateTime :: DateTime -> Bool
-checkDateTime = undefined
+checkDateTime (DateTime date time _) = isValidDate date && isValidTime time
+  where
+    isValidTime :: Time -> Bool
+    isValidTime (Time (Hour h) (Minute m) (Second s)) = minMax h (0, 23) && minMax m (0, 59) && minMax s (0, 59)
+
+    isValidDate :: Date -> Bool
+    isValidDate (Date (Year y) (Month m) (Day d)) =
+      minMax (abs y) (0, 9999)
+        && minMax m (1, 12)
+        && minMax d (1, numDays)
+      where
+        numDays :: Int
+        numDays = gregorianMonthLength (fromIntegral y) m
+
+    minMax :: Int -> (Int, Int) -> Bool
+    minMax x (min, max) = x >= min && x <= max
