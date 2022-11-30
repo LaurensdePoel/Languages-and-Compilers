fc35c22352fb816abf771e21ac33fbdcd1053230
LaurensdePoel <Laurens@de-poel.com>
2022-11-30 17:22:05 +0100

    zaad

--- Exercise1/src/Calendar.hs
+++ Exercise1/src/Calendar.hs
@@ -3,6 +3,7 @@ module Calendar where
 import DateTime
 import ParseLib.Abstract
 import Prelude hiding (sequence, ($>), (*>), (<$), (<*))
+import Data.Time.Calendar (isLeapYear, gregorianMonthLength)
 
 -- Exercise 6
 
@@ -25,7 +26,7 @@ import Prelude hiding (sequence, ($>), (*>), (<$), (<*))
 data Calendar = Calendar
   { prodid :: String, -- calander id?
     version :: String,
-    event :: [Event]
+    events :: [Event]
   }
   deriving (Eq, Ord, Show)
 
@@ -59,6 +60,23 @@ data TokenKey
   | TVersion
   deriving (Eq, Ord, Show)
 
+
+dateTimeToSeconds :: DateTime -> Int
+dateTimeToSeconds DateTime {date=Date {year=_year, month=_month, day=_day}, time= Time {hour = _hour, minute = _minute, second = _second}} = 
+    runSecond _second +
+    runMinute _minute * 60 + 
+    runHour _hour * 60 *60 + 
+    runDay _day * 24 * 60 * 60 + 
+    nrDaysOfMonth _year  _month * 24 * 60 * 60 +
+    nrDaysOfYear (runYear _year) * 24 * 60 * 60 
+
+
+nrDaysOfMonth :: Year -> Month -> Int
+nrDaysOfMonth year month = gregorianMonthLength (fromIntegral (runYear year)) (runMonth month)
+nrDaysOfYear :: Int -> Int
+nrDaysOfYear year | isLeapYear (fromIntegral year - 1970) = 366
+              | otherwise = 365
+
 tDateTime, tText, tKey :: Parser Char Token
 tDateTime = TDateTime <$> parseDateTime
 tText = TText <$> greedy1 (satisfy stillChars)
@@ -85,12 +103,105 @@ scanCalendar = greedy anyToken <* eof
 dateTime :: Parser Token DateTime
 dateTime = fromDateTime <$> satisfy isDateTime
 
+isDateTime :: Token -> Bool
+isDateTime (TDateTime _) = True
+isDateTime _ = False
+
+fromDateTime :: Token -> DateTime
+fromDateTime (TDateTime x) = x
+fromDateTime _ = error "fromDateTime"
+
+text :: Parser Token String
+text = fromText <$> satisfy isText
+
+isText :: Token -> Bool
+isText (TText _) = True
+isText _ = False
+
+fromText :: Token -> String
+fromText (TText x) = x
+fromText _ = error "fromText"
+
+keyWord :: Parser Token TokenKey
+keyWord = fromKey <$> satisfy isKey
+
+isKey :: Token -> Bool
+isKey (TKey _) = True
+isKey _ = False
+
+fromKey :: Token -> TokenKey
+fromKey (TKey x) = x
+fromKey _ = error "fromKey"
+
+
+
 parseCalendar :: Parser Token Calendar
-parseCalendar = parse
+parseCalendar = undefined
 
 recognizeCalendar :: String -> Maybe Calendar
 recognizeCalendar s = run scanCalendar s >>= run parseCalendar
 
 -- Exercise 8
+testStartDate :: DateTime
+testStartDate = DateTime (Date (Year 1997) (Month 07) (Day 14)) (Time (Hour 17) (Minute 00) (Second 00)) True
+
+testInbetweenDate :: DateTime
+testInbetweenDate = DateTime (Date (Year 1997) (Month 07) (Day 14)) (Time (Hour 20) (Minute 05) (Second 10)) True
+
+testEndDate :: DateTime
+testEndDate = DateTime (Date (Year 1997) (Month 07) (Day 15)) (Time (Hour 03) (Minute 00) (Second 00)) True
+
+testOutSideDate :: DateTime
+testOutSideDate = DateTime (Date (Year 1997) (Month 05) (Day 15)) (Time (Hour 04) (Minute 00) (Second 00)) True
+
+testCalendar :: Calendar
+testCalendar = Calendar {
+  prodid= "-//hacksw/handcal//NONSGML v1.0//EN", 
+  version= "2.0", 
+  events= [Event {
+    dtstamp= DateTime (Date (Year 1997) (Month 06) (Day 10)) (Time (Hour 17) (Minute 23) (Second 45)) True,
+    uid=  "19970610T172345Z-AF23B2@example.com", 
+    dtstart= DateTime (Date (Year 1997) (Month 07) (Day 14)) (Time (Hour 17) (Minute 00) (Second 00)) True, 
+    dtend= DateTime (Date (Year 1997) (Month 07) (Day 15)) (Time (Hour 03) (Minute 00) (Second 00)) True, 
+    description="", 
+    summary= "Bastille", 
+    location=""},
+    Event {
+    dtstamp= DateTime (Date (Year 1997) (Month 02) (Day 10)) (Time (Hour 07) (Minute 23) (Second 45)) True,
+    uid=  "1997061175Z-AF23B2@example.com", 
+    dtstart= DateTime (Date (Year 1997) (Month 07) (Day 14)) (Time (Hour 17) (Minute 00) (Second 00)) True, 
+    dtend= DateTime (Date (Year 1997) (Month 07) (Day 14)) (Time (Hour 05) (Minute 00) (Second 00)) True, 
+    description="", 
+    summary= "Bastille  Party", 
+    location=""}]
+    }
+
 printCalendar :: Calendar -> String
-printCalendar = undefined
+printCalendar Calendar {prodid=_prodid, version=_version, events =_events} = 
+  "BEGIN:VCALENDAR \r\n" ++
+  "PRODID:" ++ _prodid ++ "\r\n" ++
+  "VERSION:" ++ _version ++ "\r\n" ++
+  concatMap printEvent _events ++
+  "END:VCALENDAR \r\n"
+  where
+    printEvent :: Event -> String
+    printEvent Event {
+      dtstamp=_dtstamp, 
+      uid=_uid, 
+      dtstart=_dtstart, 
+      dtend=_dtend, 
+      description=_description, 
+      summary=_summary, 
+      location=_location} =
+        "BEGIN:VEVENT \r\n" ++
+        "SUMMARY:" ++ _summary ++ "\r\n" ++
+        "UID:" ++ _uid ++ "\r\n" ++
+        "LOCATION:" ++ _location ++ "\r\n" ++
+        "DESCRIPTION:" ++ _description ++ "\r\n" ++
+        "DTSTAMP:" ++ printDateTime _dtstamp ++ "\r\n" ++
+        "DTSTART:" ++ printDateTime _dtstart ++ "\r\n" ++
+        "DTEND:" ++ printDateTime _dtend ++ "\r\n" ++
+        "END:VEVENT \r\n"
+
+
+
--- Exercise1/src/Features.hs
+++ Exercise1/src/Features.hs
@@ -3,20 +3,178 @@ module Features where
 import Calendar
 import DateTime
 import Text.PrettyPrint.Boxes
+import Data.List as P
+import Data.Map as M
 
 -- Exercise 9
 countEvents :: Calendar -> Int
-countEvents = undefined
+countEvents Calendar {events = _events} = length _events
 
 findEvents :: DateTime -> Calendar -> [Event]
-findEvents = undefined
+findEvents datetime Calendar {events = _events} = 
+    P.filter (isTimeBetweenEvent datetime) _events
+    
+isTimeBetweenEvent:: DateTime -> Event -> Bool
+isTimeBetweenEvent checkDate Event {dtstart=_dtstart, dtend=_dtend} = 
+    checkDate >= _dtstart && checkDate < _dtend
 
 checkOverlapping :: Calendar -> Bool
-checkOverlapping = undefined
+checkOverlapping Calendar {events = _events} = 
+    any (\event@Event {dtstart=_dtstart, dtend=_dtend} -> 
+        any (\_event -> isTimeBetweenEvent _dtstart _event && _event /= event ) _events || 
+        any (\_event -> isTimeBetweenEvent _dtend _event && _event /= event) _events) _events
 
 timeSpent :: String -> Calendar -> Int
-timeSpent = undefined
+timeSpent checkSummary Calendar {events = _events} = P.foldr (\x res -> res + eventTime x) 0 filteredEvents `div` 60
+    where 
+        filteredEvents = P.filter (\Event {summary = _summary} -> _summary == checkSummary) _events
+        eventTime :: Event -> Int
+        eventTime Event {dtstart=_dtstart, dtend=_dtend} = dateTimeToSeconds _dtend - dateTimeToSeconds _dtstart
+
 
 -- Exercise 10
 ppMonth :: Year -> Month -> Calendar -> String
-ppMonth = undefined
+ppMonth year month calendar = render $ createDayBox "1 1 12:00-13:00 14:00-15:00"
+    where
+        -- createMonth = columns left 5 5 "test kaljfklj akdjf\ntest2" -- nrDaysOfMonth (runYear year) (runMonth month)
+        fixedWidth :: Int
+        fixedWidth = 20
+        boxBorder :: String
+        boxBorder = replicate fixedWidth '-'
+        columnBorder :: [Int] -> String
+        columnBorder boxHeights = (concatMap (\height -> "+" ++ replicate height '|') boxHeights) ++ "+"
+        createDayBox :: String -> Box
+        createDayBox dayInfo = para top fixedWidth (boxBorder ++ boxInfo ++boxBorder)
+            where
+                elements = words dayInfo
+                boxInfo = concatMap (\element -> element ++ replicate (fixedWidth - length element) ' ') elements
+        
+
+
+
+ppMyBox :: Int -> Int -> String -> String
+ppMyBox sizeX sizeY message = row sizeX ++ fillfield message
+    where
+        row sizeX = "+" ++ replicate (sizeX -2) '-' ++ "+\n"
+        fillfield fieldVal = fieldVal ++ replicate (length fieldVal - sizeX) ' '
+-- get a [Box] that contains all days
+
+-- create week = [Box] that are glued together with hcat
+
+-- create a month by putting the weeks together
+
+printLine :: String
+printLine = concat (replicate 7 ("+" ++ replicate 13 '-')) ++ "+" ++ "\n"
+
+-- * Exercise 3
+
+printField :: Int -> String -> String
+printField n xs= xs ++ whiteSpace
+  where
+    numberOfWhitespace = n - length xs
+    whiteSpace = replicate numberOfWhitespace ' '
+
+-- -- * Exercise 4
+
+printRow :: [String] -> String
+printRow xs = "|" ++ intercalate "|" (P.map (printField 13) xs) ++ "|" ++ "\n"
+
+printDayNr:: [Int] -> String
+printDayNr xs = "|" ++ intercalate "|" (P.map (printDay 13 ) xs) ++ "|" ++ "\n"
+
+printDay :: Int -> Int -> String
+printDay n dayNr | dayNr > 0 = show dayNr ++ whiteSpace
+                 | otherwise = "             "
+  where
+    numberOfWhitespace | dayNr < 10 = n - 1
+                       | otherwise = n - 2
+    whiteSpace = replicate numberOfWhitespace ' '
+
+-- -- * Exercise 5
+
+
+-- -- * Exercise 6
+
+printTable :: Calendar -> String
+printTable Calendar {events=_events} = 
+    printLine ++ 
+    printDayNr week1 ++ 
+    printLine ++
+    printDayNr week2 ++ 
+    printLine ++ 
+    printDayNr week3 ++ 
+    printLine ++
+    printDayNr week4 ++
+    printLine ++
+    printDayNr week5 ++
+    printLine ++
+    printWeek week2
+
+        where
+          allEvents = createCalendarDays tmpYear tmpMonth testCalendar
+          allDays = createDaysheading (Year 2000) (Month 7) -- Dates tmp
+          week1 = P.take 7 allDays
+          week2 = P.take 7 $ P.drop 7 allDays
+          week3 = P.take 7 $ P.drop 14 allDays
+          week4 = P.take 7 $ P.drop 21 allDays
+          week5 | length tmpWeek5 < 7 = tmpWeek5 ++ (P.take (7 - length tmpWeek5) [0,0..])
+                | otherwise = P.drop 29 allDays
+          tmpWeek5 = P.drop 29 allDays
+
+          printWeek :: [Int] -> [String]
+          printWeek daysNrs = results
+          where
+            results = map (flip getDayEvents allEvents) daysNrs
+
+
+-- ["",""]
+-- [jaklf, akjflas]
+-- ["",""]
+-- ["",""]
+-- ["",""]
+
+
+-- maand jaar -> maand 31 -> [1,2,3....,31]
+-- [1,2,3....,31] % 7 -> ["1", "2", "3", "4","5","6","7"] 
+--       [30,31,"","","",]
+tmpYear = Year 1997
+tmpMonth = Month 7
+
+createDaysheading :: Year -> Month -> [Int]
+createDaysheading year month = getAllDays
+    where
+        getAllDays = P.take amountOfDays [1,2..]
+        amountOfDays = nrDaysOfMonth year month 
+
+createCalendarDays :: Year -> Month -> Calendar -> Map Int [String]
+createCalendarDays year' month' Calendar {events = _events} = P.foldr bla M.empty _events
+    where
+        bla Event {dtstart=_dtstart, dtend=_dtend} res 
+            -- | year $ date _dtstart /= year' && month $ date _dtstart /= month' = res
+            | year ( date _dtstart) /= year' || month (date _dtstart) /= month' = res
+            | otherwise = 
+                case M.lookup (runDay $ day $ date _dtstart) res of
+                    Nothing -> M.insert (runDay $ day $ date _dtstart) [dateTimeToString _dtstart ++ " - " ++ dateTimeToString _dtend] res
+                    Just x -> M.insert (runDay $ day $ date _dtstart) ((dateTimeToString _dtstart ++ " - " ++ dateTimeToString _dtend) : x) res
+
+        dateTimeToString :: DateTime -> String
+        dateTimeToString DateTime {time= Time {hour = _hour, minute = _minute}} = showNum (runHour _hour) ++ ":" ++ showNum (runMinute _minute)
+            where
+                showNum :: Int -> String
+                showNum x | x < 9 = "0" ++ show x
+                        | otherwise = show x 
+                        
+getDayEvents :: Int -> Map Int [String] -> [String]
+getDayEvents key map = 
+    case M.lookup key map of
+        Nothing -> []
+        Just x -> x
+
+-- logica samenvoegen
+
+-- printTable table@(header : rows) = [printLine width] ++ header' ++ [printLine width] ++ rows' ++ [printLine width]
+--   where
+--     width = columnWidths table
+--     headerU = map (map toUpper) header
+--     header' = map printRow [zip width headerU]
+--     rows' = concatMap (\row -> map printRow [zip width row]) rows
