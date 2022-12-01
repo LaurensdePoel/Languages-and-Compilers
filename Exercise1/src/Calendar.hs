16c33b3001e54785c26030d7a2cc7b59200d0264
markg2000 <mrk3600@gmail.com>
2022-12-01 12:40:57 +0100

    new structure

--- Exercise1/src/Calendar.hs
+++ Exercise1/src/Calendar.hs
@@ -25,22 +25,37 @@ import Prelude hiding (sequence, ($>), (*>), (<$), (<*))
 -}
 
 data Calendar = Calendar
-  { prodid :: String, -- calander id?
-    version :: String,
+  { calprop :: [CalProp],
     events :: [Event]
   }
   deriving (Eq, Ord, Show)
 
+data CalProp = PRODID String | VERSION String deriving (Eq, Ord, Show)
+
+-- newtype ProdID = PRODID String
+
+-- newtype Version = VERSION String
+
 data Event = Event
-  { dtstamp :: DateTime,
-    uid :: String,
-    dtstart :: DateTime,
-    dtend :: DateTime,
-    description :: String,
-    summary :: String,
-    location :: String
+  { eventprops :: [EventProp]
   }
   deriving (Eq, Ord, Show)
+
+data EventProp = DTSTAMP DateTime | UID String | DTSTART DateTime | DTEND DateTime | DESCRIPTION String | SUMMARY String | LOCATION String deriving (Eq, Ord, Show)
+
+-- newtype DtStamp = DTSTAMP DateTime
+
+-- newtype Uid = UID String
+
+-- newtype DtStart = DTSTART DateTime
+
+-- newtype DtEnd = DTEND DateTime
+
+-- newtype Description = DESCRIPTION String
+
+-- newtype Summary = SUMMARY String
+
+-- newtype Location = LOCATION String
 
 -- Exercise 7
 data Token
@@ -59,6 +74,10 @@ data TokenKey
   | TLocation
   | TProdID
   | TVersion
+  | TBEGIN
+  | TEND
+  | TVCALENDAR
+  | TVEVENT
   deriving (Eq, Ord, Show)
 
 dateTimeToSeconds :: DateTime -> Int
@@ -94,6 +113,10 @@ tKey =
     <|> TKey TLocation <$ token "LOCATION:"
     <|> TKey TProdID <$ token "PRODID:"
     <|> TKey TVersion <$ token "VERSION:"
+    <|> TKey TBEGIN <$ token "BEGIN:"
+    <|> TKey TEND <$ token "END:"
+    <|> TKey TVCALENDAR <$ token "VCALENDAR"
+    <|> TKey TVEVENT <$ token "VEVENT"
 
 anyToken :: Parser Char Token
 anyToken = tDateTime <|> tKey <|> tText
@@ -101,7 +124,7 @@ anyToken = tDateTime <|> tKey <|> tText
 scanCalendar :: Parser Char [Token]
 scanCalendar = greedy anyToken -- <* eof
 
-tes = run scanCalendar "PRODID:2\nVERSION:1\nDTSTAMP:19970610T172345Z\nUID:test\nDTSTART:19970610T172345Z\nDTEND:19970710T172345Z\nDESCRIPTION:test\nSUMMARY:test\nLOCATION:test\n" -- "DTSTART:19970610T172345ZDTEND:19970610T172345Z"
+tes = run scanCalendar "BEGIN:VCALENDAR\nPRODID:2\nVERSION:1\nBEGIN:VEVENT\nDTSTAMP:19970610T172345Z\nUID:test\nDTSTART:19970610T172345Z\nDTEND:19970710T172345Z\nDESCRIPTION:test\nSUMMARY:test\nLOCATION:test\nEND:VEVENT\nEND:VCALENDAR\n" -- "DTSTART:19970610T172345ZDTEND:19970610T172345Z"
 
 tes3 = run scanCalendar "PRODID:test123\nVERSION:2.0\n"
 
@@ -143,11 +166,11 @@ fromKey :: Token -> TokenKey
 fromKey (TKey x) = x
 fromKey _ = error "fromKey"
 
-event :: Parser Token Event
-event = Event <$ keyWord <*> dateTime <* keyWord <*> text <* keyWord <*> dateTime <* keyWord <*> dateTime <* keyWord <*> text <* keyWord <*> text <* keyWord <*> text
+-- event :: Parser Token Event
+-- event = Event <$ keyWord <*> dateTime <* keyWord <*> text <* keyWord <*> dateTime <* keyWord <*> dateTime <* keyWord <*> text <* keyWord <*> text <* keyWord <*> text
 
 parseCalendar :: Parser Token Calendar
-parseCalendar = Calendar <$ keyWord <*> text <* keyWord <*> text <*> many event
+parseCalendar = undefined -- Calendar <$ keyWord <*> text <* keyWord <*> text <*> many event
 
 recognizeCalendar :: String -> Maybe Calendar
 recognizeCalendar s = run scanCalendar s >>= run parseCalendar
@@ -168,77 +191,130 @@ testOutSideDate = DateTime (Date (Year 1997) (Month 05) (Day 15)) (Time (Hour 04
 testCalendar :: Calendar
 testCalendar =
   Calendar
-    { prodid = "-//hacksw/handcal//NONSGML v1.0//EN",
-      version = "2.0",
+    { calprop = [PRODID "-//hacksw/handcal//NONSGML v1.0//EN", VERSION "2.0"],
       events =
         [ Event
-            { dtstamp = DateTime (Date (Year 1997) (Month 06) (Day 10)) (Time (Hour 17) (Minute 23) (Second 45)) True,
-              uid = "19970610T172345Z-AF23B2@example.com",
-              dtstart = DateTime (Date (Year 1997) (Month 07) (Day 14)) (Time (Hour 17) (Minute 00) (Second 00)) True,
-              dtend = DateTime (Date (Year 1997) (Month 07) (Day 15)) (Time (Hour 03) (Minute 00) (Second 00)) True,
-              description = "",
-              summary = "Bastille",
-              location = ""
-            },
-          Event
-            { dtstamp = DateTime (Date (Year 1997) (Month 02) (Day 10)) (Time (Hour 07) (Minute 23) (Second 45)) True,
-              uid = "1997061175Z-AF23B2@example.com",
-              dtstart = DateTime (Date (Year 1997) (Month 07) (Day 14)) (Time (Hour 17) (Minute 00) (Second 00)) True,
-              dtend = DateTime (Date (Year 1997) (Month 07) (Day 14)) (Time (Hour 05) (Minute 00) (Second 00)) True,
-              description = "",
-              summary = "Bastille  Party",
-              location = ""
+            { eventprops =
+                [ DTSTAMP (DateTime (Date (Year 1997) (Month 06) (Day 10)) (Time (Hour 17) (Minute 23) (Second 45)) True),
+                  UID "19970610T172345Z-AF23B2@example.com",
+                  DTSTART (DateTime (Date (Year 1997) (Month 07) (Day 14)) (Time (Hour 17) (Minute 00) (Second 00)) True),
+                  DTEND (DateTime (Date (Year 1997) (Month 07) (Day 14)) (Time (Hour 19) (Minute 00) (Second 00)) True),
+                  SUMMARY "Bastille"
+                ]
             }
         ]
     }
 
+testEvent :: Event
+testEvent =
+  Event
+    { eventprops =
+        [ DTSTAMP (DateTime (Date (Year 1997) (Month 06) (Day 10)) (Time (Hour 17) (Minute 23) (Second 45)) True),
+          UID "19970610T172345Z-AF23B2@example.com",
+          DTEND (DateTime (Date (Year 1997) (Month 07) (Day 14)) (Time (Hour 19) (Minute 20) (Second 00)) True),
+          DTSTART (DateTime (Date (Year 1997) (Month 07) (Day 14)) (Time (Hour 17) (Minute 00) (Second 00)) True),
+          SUMMARY "Bastille"
+        ]
+    }
+
+-- { prodid = "-//hacksw/handcal//NONSGML v1.0//EN",
+--   version = "2.0",
+--   events =
+--     [ Event
+--         { dtstamp = DateTime (Date (Year 1997) (Month 06) (Day 10)) (Time (Hour 17) (Minute 23) (Second 45)) True,
+--           uid = "19970610T172345Z-AF23B2@example.com",
+--           dtstart = DateTime (Date (Year 1997) (Month 07) (Day 14)) (Time (Hour 17) (Minute 00) (Second 00)) True,
+--           dtend = DateTime (Date (Year 1997) (Month 07) (Day 15)) (Time (Hour 03) (Minute 00) (Second 00)) True,
+--           description = "",
+--           summary = "Bastille",
+--           location = ""
+--         },
+--       Event
+--         { dtstamp = DateTime (Date (Year 1997) (Month 02) (Day 10)) (Time (Hour 07) (Minute 23) (Second 45)) True,
+--           uid = "1997061175Z-AF23B2@example.com",
+--           dtstart = DateTime (Date (Year 1997) (Month 07) (Day 14)) (Time (Hour 17) (Minute 00) (Second 00)) True,
+--           dtend = DateTime (Date (Year 1997) (Month 07) (Day 14)) (Time (Hour 05) (Minute 00) (Second 00)) True,
+--           description = "",
+--           summary = "Bastille  Party",
+--           location = ""
+--         }
+--     ]
+-- }
+
 printCalendar :: Calendar -> String
-printCalendar Calendar {prodid = _prodid, version = _version, events = _events} =
+printCalendar Calendar {calprop = _calprop, events = _events} =
   "BEGIN:VCALENDAR \r\n"
-    ++ "PRODID:"
-    ++ _prodid
-    ++ "\r\n"
-    ++ "VERSION:"
-    ++ _version
-    ++ "\r\n"
-    ++ concatMap printEvent _events
+    ++ concatMap calPropToString _calprop
+    ++ concatMap eventToString _events
     ++ "END:VCALENDAR \r\n"
   where
-    printEvent :: Event -> String
-    printEvent
-      Event
-        { dtstamp = _dtstamp,
-          uid = _uid,
-          dtstart = _dtstart,
-          dtend = _dtend,
-          description = _description,
-          summary = _summary,
-          location = _location
-        } =
-        "BEGIN:VEVENT \r\n"
-          ++ "SUMMARY:"
-          ++ _summary
-          ++ "\r\n"
-          ++ "UID:"
-          ++ _uid
-          ++ "\r\n"
-          ++ "LOCATION:"
-          ++ _location
-          ++ "\r\n"
-          ++ "DESCRIPTION:"
-          ++ _description
-          ++ "\r\n"
-          ++ "DTSTAMP:"
-          ++ printDateTime _dtstamp
-          ++ "\r\n"
-          ++ "DTSTART:"
-          ++ printDateTime _dtstart
-          ++ "\r\n"
-          ++ "DTEND:"
-          ++ printDateTime _dtend
-          ++ "\r\n"
-          ++ "END:VEVENT \r\n"
+    calPropToString :: CalProp -> String
+    calPropToString (PRODID x) = "PRODID:" ++ x ++ "\r\n"
+    calPropToString (VERSION x) = "VERSION:" ++ x ++ "\r\n"
+
+    eventToString :: Event -> String
+    eventToString Event {eventprops = _eventprops} =
+      "BEGIN:VEVENT \r\n"
+        ++ concatMap eventPropToString _eventprops
+        ++ "END:VEVENT \r\n"
+      where
+        eventPropToString :: EventProp -> String
+        eventPropToString (DTSTAMP x) = "DTSTAMP:" ++ printDateTime x ++ "\r\n"
+        eventPropToString (UID x) = "UID:" ++ x ++ "\r\n"
+        eventPropToString (DTSTART x) = "DTSTART:" ++ printDateTime x ++ "\r\n"
+        eventPropToString (DTEND x) = "DTEND:" ++ printDateTime x ++ "\r\n"
+        eventPropToString (SUMMARY x) = "SUMMARY:" ++ x ++ "\r\n"
+        eventPropToString (DESCRIPTION x) = "DESCRIPTION:" ++ x ++ "\r\n"
+        eventPropToString (LOCATION x) = "LOCATION:" ++ x ++ "\r\n"
+
+-- printCalendar Calendar {prodid = _prodid, version = _version, events = _events} =
+--   "BEGIN:VCALENDAR \r\n"
+--     ++ "PRODID:"
+--     ++ _prodid
+--     ++ "\r\n"
+--     ++ "VERSION:"
+--     ++ _version
+--     ++ "\r\n"
+--     ++ concatMap printEvent _events
+--     ++ "END:VCALENDAR \r\n"
+--   where
+--     printEvent :: Event -> String
+--     printEvent
+--       Event
+--         { dtstamp = _dtstamp,
+--           uid = _uid,
+--           dtstart = _dtstart,
+--           dtend = _dtend,
+--           description = _description,
+--           summary = _summary,
+--           location = _location
+--         } =
+--         "BEGIN:VEVENT \r\n"
+--           ++ "SUMMARY:"
+--           ++ _summary
+--           ++ "\r\n"
+--           ++ "UID:"
+--           ++ _uid
+--           ++ "\r\n"
+--           ++ "LOCATION:"
+--           ++ _location
+--           ++ "\r\n"
+--           ++ "DESCRIPTION:"
+--           ++ _description
+--           ++ "\r\n"
+--           ++ "DTSTAMP:"
+--           ++ printDateTime _dtstamp
+--           ++ "\r\n"
+--           ++ "DTSTART:"
+--           ++ printDateTime _dtstart
+--           ++ "\r\n"
+--           ++ "DTEND:"
+--           ++ printDateTime _dtend
+--           ++ "\r\n"
+--           ++ "END:VEVENT \r\n"
 
 tes5 = case tes2 of
   Nothing -> ""
   Just x -> printCalendar x
+
+tes6 = printCalendar testCalendar
